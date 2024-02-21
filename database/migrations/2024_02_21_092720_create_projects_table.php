0b4c79f4309319933173aae0f6b10bca8b50ef49
Gabriele <gabriele.palma.bool@gmail.com>
2024-02-21 10:42:52 +0100

    prova

---
+++ database/migrations/2024_02_21_092720_create_projects_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nome_autore',20);
+            $table->string('titolo_progetto',60) -> unique();
+            $table->string('img_riferimento');
+            $table->string('descrizione') -> nullable();
+            $table->date('data_pubblicazione');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
